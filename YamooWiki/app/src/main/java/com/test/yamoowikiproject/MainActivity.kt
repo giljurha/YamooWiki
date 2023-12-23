3227e0bff57e6e453268fde4c8fa9833a15a4105
DESKTOP-2IJ6G78\gilju <giljurha93@gmail.com>
2023-12-23 15:26:33 +0900

    feat. login xml 수정

--- YamooWiki/app/src/main/java/com/test/yamoowikiproject/MainActivity.kt
+++ YamooWiki/app/src/main/java/com/test/yamoowikiproject/MainActivity.kt
@@ -2,12 +2,18 @@ package com.test.yamoowikiproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.test.yamoowikiproject.ui.user.LoginFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        
+        val fragment = LoginFragment()
+        val manager = supportFragmentManager
+        val transaction = manager.beginTransaction()
+
+        transaction.add(R.id.mainLayout, fragment)
+        transaction.commit()
     }
 }
