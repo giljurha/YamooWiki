a483de515270f4ec33ac53b1d85366bc974b0258
DESKTOP-2IJ6G78\gilju <giljurha93@gmail.com>
2023-12-23 21:42:20 +0900

    feat. 회원가입 xml 구현

--- YamooWiki/app/src/main/java/com/test/yamoowikiproject/MainActivity.kt
+++ YamooWiki/app/src/main/java/com/test/yamoowikiproject/MainActivity.kt
@@ -3,13 +3,14 @@ package com.test.yamoowikiproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.test.yamoowikiproject.ui.user.LoginFragment
+import com.test.yamoowikiproject.ui.user.SignupFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val fragment = LoginFragment()
+        val fragment = SignupFragment()
         val manager = supportFragmentManager
         val transaction = manager.beginTransaction()
 
