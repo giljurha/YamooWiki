65324b88243abed4a6ae9e87b61ba174e1f28330
Ganghyungoo <104668071+Ganghyungoo@users.noreply.github.com>
2023-12-26 13:27:53 +0900

    feat: xml 변경 및 코드 리뷰

--- YamooWiki/app/src/main/java/com/test/yamoowikiproject/MainActivity.kt
+++ YamooWiki/app/src/main/java/com/test/yamoowikiproject/MainActivity.kt
@@ -2,19 +2,22 @@ package com.test.yamoowikiproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.test.yamoowikiproject.databinding.ActivityMainBinding
 import com.test.yamoowikiproject.ui.user.LoginFragment
 import com.test.yamoowikiproject.ui.user.SignupFragment
 
 class MainActivity : AppCompatActivity() {
+    lateinit var activityMainbinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        activityMainbinding = ActivityMainBinding.inflate(layoutInflater)
 
         val fragment = SignupFragment()
         val manager = supportFragmentManager
         val transaction = manager.beginTransaction()
 
-        transaction.add(R.id.mainLayout, fragment)
+        transaction.add(R.id.mainContainer, fragment)
         transaction.commit()
     }
 }
--- YamooWiki/app/src/main/java/com/test/yamoowikiproject/ui/user/LoginFragment.kt
+++ YamooWiki/app/src/main/java/com/test/yamoowikiproject/ui/user/LoginFragment.kt
@@ -5,18 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.test.yamoowikiproject.MainActivity
 import com.test.yamoowikiproject.R
+import kotlin.concurrent.fixedRateTimer
 
 
 class LoginFragment : Fragment() {
-
-
+    lateinit var mainActivity:MainActivity
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+
+
+
         return inflater.inflate(R.layout.fragment_login, container, false)
+
+
     }
 
 
