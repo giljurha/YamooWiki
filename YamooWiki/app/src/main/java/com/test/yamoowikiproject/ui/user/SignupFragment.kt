0902d66c306017b2ec5b02de959528ddca910c41
DESKTOP-2IJ6G78\gilju <giljurha93@gmail.com>
2024-03-06 11:19:05 +0900

    feat: 갤러리에서 사진 가져오기 구현

--- YamooWiki/app/src/main/java/com/test/yamoowikiproject/ui/user/SignupFragment.kt
+++ YamooWiki/app/src/main/java/com/test/yamoowikiproject/ui/user/SignupFragment.kt
@@ -1,13 +1,29 @@
 package com.test.yamoowikiproject.ui.user
 
+import android.app.Activity
 import android.content.DialogInterface
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import com.bumptech.glide.Glide
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.test.yamoowikiproject.R
+
 import com.test.yamoowikiproject.databinding.FragmentSignupBinding
 import com.test.yamoowikiproject.db.YamooWikiDatabase
 import com.test.yamoowikiproject.db.UserEntity
@@ -16,6 +32,9 @@ import com.test.yamoowikiproject.viewmodel.MainViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
 
 
 class SignupFragment : Fragment() {
@@ -40,6 +59,9 @@ class SignupFragment : Fragment() {
 
         fragmentSignupBinding.confirmButton.setOnClickListener {
             confirm()
+        }
+        fragmentSignupBinding.userProfileImage.setOnClickListener{
+            selectGallery()
         }
     }
 
@@ -88,6 +110,70 @@ class SignupFragment : Fragment() {
         }
         builder.show()
     }
+
+    val REQUEST_IMAGE_CAPTURE = 1
+    lateinit var curPhotoPath: String
+
+//    private fun setPermission() {
+//        val permission = object : PermissionListener {
+//            override fun onPermissionGranted() {
+//                Toast.makeText(requireContext(), "권한이 허용 되었습니다.", Toast.LENGTH_SHORT).show()
+//            }
+//
+//            override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+//                Toast.makeText(requireContext(), "권한이 거부 되었습니다.", Toast.LENGTH_SHORT).show()
+//            }
+//        }
+//        TedPermission.with(requireContext())
+//            .setPermissionListener(permission)
+//            .setRationaleMessage("카메라 앱을 사용하시려면 권한을 허용해주세요.")
+//            .setDeniedMessage("권한을 거부하셨습니다. [앱 설정] -> [권한] 항목에서 허용해주세요.")
+//            .setPermissions(android.Manifest.permission.WRITE_EXTERNAL_STORAGE, android.Manifest.permission.CAMERA)
+//            .check()
+//    }
+
+
+    private fun selectGallery() {
+
+        val readPermission = ContextCompat.checkSelfPermission(
+            requireContext(),
+            android.Manifest.permission.READ_EXTERNAL_STORAGE
+        )
+
+        if (readPermission == PackageManager.PERMISSION_DENIED) { //권한이 없을 시 권한 요청
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 100
+            )
+        } else {
+            //권한이 있을 경우 갤러리 실행
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.setDataAndType(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                "image/*"
+            )
+            imageResult.launch(intent)
+        }
+    }
+
+
+
+
+    var imageResult = registerForActivityResult( //oncreate에서만 정의가능 -> 모듈화 불가능
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == Activity.RESULT_OK){
+            Glide.with(this)
+                .load(result.data?.data)
+                .override(200,200)
+                .into(fragmentSignupBinding.userProfileImage)
+        }
+
+
+    }
+
+
+
+
+
 }
-
-
