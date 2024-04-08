359fa6ed9ccc68375cfe72c4618881bca65e05d5
DESKTOP-2IJ6G78\gilju <giljurha93@gmail.com>
2024-04-08 17:23:49 +0900

    feat: UserActivity 생성

---
+++ YamooWiki/app/src/main/java/com/test/yamoowikiproject/ui/user/UserActivity.kt
@@ -0,0 +1,15 @@
+package com.test.yamoowikiproject.ui.user
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.test.yamoowikiproject.R
+
+class UserActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
