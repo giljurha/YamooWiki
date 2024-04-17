f3f118efed66996549d41a609aaf5d96a35437f9
DESKTOP-2IJ6G78\gilju <giljurha93@gmail.com>
2024-04-17 16:16:07 +0900

    refactor: db 메서드 이름 수정

--- YamooWiki/app/src/main/java/com/test/yamoowikiproject/db/UserDao.kt
+++ YamooWiki/app/src/main/java/com/test/yamoowikiproject/db/UserDao.kt
@@ -19,7 +19,7 @@ interface UserDao {
     fun deleteUser(userEntity: UserEntity)
 
     @Query("SELECT * FROM UserEntity WHERE userId = :userId AND userPassword = :password LIMIT 1")
-    fun getUserInfo(userId: String, password: String): UserEntity?
+    fun getUserIdPassword(userId: String, password: String): UserEntity?
 
     @Query("SELECT * FROM UserEntity WHERE userId = :userId")
     fun getUserId(userId: String): UserEntity?
