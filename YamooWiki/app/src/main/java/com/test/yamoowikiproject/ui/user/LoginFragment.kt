b308a2177a7e79e95fdd79bda6ef31406bb2b062
DESKTOP-2IJ6G78\gilju <giljurha93@gmail.com>
2024-04-28 16:49:32 +0900

    feat: loginFragment 에러메세지 추가

--- YamooWiki/app/src/main/java/com/test/yamoowikiproject/ui/user/LoginFragment.kt
+++ YamooWiki/app/src/main/java/com/test/yamoowikiproject/ui/user/LoginFragment.kt
@@ -9,9 +9,12 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.test.yamoowikiproject.databinding.DialogErrorBinding
 import com.test.yamoowikiproject.databinding.FragmentLoginBinding
 import com.test.yamoowikiproject.db.UserEntity
 import com.test.yamoowikiproject.ui.main.FragmentType
+import com.test.yamoowikiproject.ui.user.model.SignupErrorState
 import com.test.yamoowikiproject.viewmodel.LoginViewModel
 import com.test.yamoowikiproject.viewmodel.MainViewModel
 
@@ -39,7 +42,8 @@ class LoginFragment : Fragment() {
                 mainViewModel.changeFragmentType(fragmentType = FragmentType.HOME)
                 mainViewModel.changeStateBottomNavigaitonView(fragmentType = FragmentType.HOME)
                 loginViewModel.loginUserEntity.value?.let {
-                    saveSharedPreferences(it, requireContext()) }
+                    saveSharedPreferences(it, requireContext())
+                }
             }
         }
         initViews()
@@ -49,14 +53,15 @@ class LoginFragment : Fragment() {
     fun initViews() {
         with(binding) {
             btnLogin.setOnClickListener {
-                val id = binding.etId.text.toString()
-                val password = binding.etPassword.text.toString()
-                loginViewModel.login(id, password, requireContext())
-                val currentUser = requireContext()
-                    .getSharedPreferences("currentUser", Context.MODE_PRIVATE)
-                    .edit()
-                    .putString("userId", id)
-                if (loginViewModel.isLogin.value == true) currentUser.commit()
+                val id = binding.etUserId.text.toString()
+                val password = binding.etUserPassword.text.toString()
+                if (id.isEmpty()) {
+                    showDialog(SignupErrorState.ID)
+                } else if (password.isEmpty()) {
+                    showDialog(SignupErrorState.PASSWORD)
+                } else {
+                    loginViewModel.login(id, password, requireContext())
+                }
             }
             tvSignup.setOnClickListener {
                 mainViewModel.changeFragmentType(fragmentType = FragmentType.SIGNUP)
@@ -71,5 +76,29 @@ class LoginFragment : Fragment() {
             .putString("userId", userEntity.userId)
             .putString("userImageUri", userEntity.userImageUri)
             .commit()
+    }
+
+    private fun showDialog(signupErrorState: SignupErrorState) {
+
+        val focusView: View? = when (signupErrorState) {
+            SignupErrorState.ID -> binding.etUserId
+            SignupErrorState.PASSWORD -> binding.etUserPassword
+            SignupErrorState.DUPLICATEDID -> binding.etUserId
+            SignupErrorState.NOTERROR -> null
+            SignupErrorState.NICKNAME -> null
+            SignupErrorState.PROFILE -> null
+            SignupErrorState.DUPLICATEDNICKNAME -> null
+        }
+
+        MaterialAlertDialogBuilder(requireContext()).run {
+            val dialogErrorBinding: DialogErrorBinding = DialogErrorBinding.inflate(layoutInflater)
+            dialogErrorBinding.textView.text = signupErrorState.message
+            setView(dialogErrorBinding.root)
+            setPositiveButton("확인") { dialog, which ->
+                focusView?.requestFocus()
+            }
+            setCancelable(true)
+            show()
+        }.setCanceledOnTouchOutside(true)
     }
 }
